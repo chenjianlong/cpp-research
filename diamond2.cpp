96a3cfb48c15e96f99a786b16f27a1248d1dba30
Jianlong Chen <jianlong99@gmail.com>
2019-04-05 12:18:35 +0800

    Add cast function pointer on diamond2

--- diamond2.cpp
+++ diamond2.cpp
@@ -8,14 +8,14 @@ public:
     int a_;
 };
 
-//class B: public A
+// class B: public A
 class B: public virtual A
 {
 public:
     int b_;
 };
 
-//class C: public A
+// class C: public A
 class C: public virtual A
 {
 public:
@@ -27,6 +27,14 @@ class D: public B, C
 public:
     int d_;
 };
+
+/*typedef void (*pfn)(A&a);
+
+void dispaly(D &d)
+{
+    cout << "dispaly: addr=" << hex << &d << endl;
+    cout << " d=" << dec << d.d_ << " a=" << d.a_ << endl;
+}*/
 
 int main(int, char *[])
 {
@@ -45,11 +53,25 @@ int main(int, char *[])
     // 虚继承时，有一份 A，
     // B C 各有一个指针指向 A
     D d;
-    cout << "sizeof(A)=" << sizeof(A) << " addr=" << hex << &(A&)d << endl;
+    // cout << "sizeof(A)=" << sizeof(A) << " addr=" << hex << &(A&)d << endl;
     //cout << "sizeof(A)=" << sizeof(A) << endl;
     cout << "sizeof(B)=" << dec << sizeof(B) << " addr=" << hex << &(B&)d << endl;
     cout << "sizeof(C)=" << dec << sizeof(C) << " addr=" << hex << &(C&)d << endl;
     cout << "sizeof(D)=" << dec << sizeof(D) << " addr=" << hex << &d << endl;
+
+    // More Effective C++ 条款 31
+    // 以下代码在虚继承的情况下会崩溃
+    // sizeof(A)=4 addr=0x7ffe4b6c2f10
+    // sizeof(B)=16 addr=0x7ffe4b6c2ef0
+    // sizeof(C)=16 addr=0x7ffe4b6c2f00
+    // sizeof(D)=40 addr=0x7ffe4b6c2ef0
+    // dispaly: addr=0x7ffe4b6c2ef0
+    //  d=0 a=1265381376
+    //  dispaly: addr=0x7ffe4b6c2f10
+    //  [1]    10659 segmentation fault (core dumped)  ./a.out
+    /*dispaly(d);
+    pfn p = (pfn) dispaly;
+    p((B&) d);*/
     return 0;
 }
 // vim: set et ts=4 sts=4 sw=4:
